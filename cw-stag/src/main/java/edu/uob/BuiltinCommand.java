bcbcf60842111b5ece494957b6f2b90e8a23ee3f
Venice <25238465+NGC0001@users.noreply.github.com>
2024-05-04 23:00:33 +0100

    implement inv

--- cw-stag/src/main/java/edu/uob/BuiltinCommand.java
+++ cw-stag/src/main/java/edu/uob/BuiltinCommand.java
@@ -1,24 +1,57 @@
 package edu.uob;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public enum BuiltinCommand implements Command {
-    INV(new String[] {"inventory", "inv"}),
-    GET(new String[] {"get"}),
-    DROP(new String[] {"drop"}),
-    GOTO(new String[] {"goto"}),
-    LOOK(new String[] {"look"}),
-    HEALTH(new String[] {"health"});
+    INV(BuiltinCommand::buildInvTask, "inventory", "inv"),
+    GET(null, "get"),
+    DROP(null, "drop"),
+    GOTO(null, "goto"),
+    LOOK(null, "look"),
+    HEALTH(null, "health");
 
-    private String[] triggers;
+    @FunctionalInterface
+    private interface TaskBuilder {
+        public Task buildTaskFromSubjects(Player player, Map<String, GameEntity> subjects);
+    }
 
-    private BuiltinCommand(String[] triggers) {
-        this.triggers = triggers;
+    private TaskBuilder builder;
+    private List<String> triggers;
+
+    private BuiltinCommand(TaskBuilder builder, String... triggers) {
+        this.builder = builder;
+        this.triggers = Arrays.asList(triggers);
     } 
 
     @Override
     public Task buildTask(Player player, List<String> playerCommand, Map<String, GameEntity> gameEntities) {
-        return null;
+        Map<String, GameEntity> subjects = Command.matchPlayerCommand(playerCommand, this.triggers, gameEntities);
+        if (subjects == null) {
+            return null;
+        }
+        return this.builder.buildTaskFromSubjects(player, subjects);
+    }
+
+    private static Task buildInvTask(Player player, Map<String, GameEntity> subjects) {
+        if (subjects == null || !subjects.isEmpty()) {
+            return null;
+        }
+        if (player == null) {
+            return null;
+        }
+        return new Task() {
+            String result;
+
+            @Override
+            public String run() {
+                this.result = "";
+                player.getInventory().forEach((name, artefact) -> {
+                    this.result += name + "\n";
+                });
+                return this.result;
+            }
+        };
     }
 }
